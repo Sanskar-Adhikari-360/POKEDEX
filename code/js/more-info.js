1daa3afe90c0fa75952abf3e1c4e1f2a5fc35822
Sanskar-Adhikari-360 <adhikarisanskar360@gmail.com>
2024-11-05 22:50:04 +0545

    added mobile responsive

--- code/js/more-info.js
+++ code/js/more-info.js
@@ -1,3 +1,18 @@
-document.getElementById("defense-section").addEventListener ('click'), () => {
-    document.getElementsByClassName("body-content-details-right-more-details-defense").style.display = "none";
+const strongDefense = document.getElementsByClassName("defense-section")
+const dexEntry =  document.getElementsByClassName("nav-dexEntry")
+console.log(strongDefense, dexEntry)
+
+const openSection =() =>{
+    dexEntry.classList.remove("hidden")
+    backdrop.classList.remove("hidden")
+    close.classList.remove("hidden")
 }
+const closeSection =() =>{
+    dexEntry.classList.add("hidden")
+    strongDefense.classList.add("hidden")
+    close.classList.add("hidden")
+}
+
+strongDefense.addEventListener("click",openSection)
+dexEntry.addEventListener("click", closeSection)
+close.addEventListener("click",closeSection)
